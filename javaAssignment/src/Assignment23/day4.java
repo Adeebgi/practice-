7589052e680745d3b77e570c78a0aba054ee7d95
User <User@10.0.0.247>
2023-04-02 23:11:59 -0400

    day4 assingment commit to the centralized repository

---
+++ javaAssignment/src/Assignment23/day4.java
@@ -0,0 +1,35 @@
+package Assignment23;
+
+public class day4 {
+	public day4() 
+	{
+		this(10,20,30,40);
+	}
+	
+	public day4(int a, int b, int c, int d)
+	{
+		
+	}
+	public day4(int a1, int b1, int c1)
+	{
+		
+	}
+	
+	public day4(int a2, int b2)
+	
+	{
+		this (45);
+		
+	}
+	public day4(int a3)
+	{
+		
+	}
+	
+	public static void main(String[] args) {
+		day4 twopara=new day4(12,13);
+		day4 threepara=new day4(13,14,15);
+		day4 defaultpara=new day4();
+		
+	}
+}
